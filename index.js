11cd7701a610295d7afce27d5e2d2cfb06657014
winston leonardo <winstonleonardo@yahoo.com.br>
2024-08-31 19:34:00 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let nome = "joao"
+let xp = 800
+let nivelDeHeroi = "ferro"
+console.log(nome)
+switch (nivelDeHeroi){
+  case "ferro" :
+  console.log("ferro")
+  break
+  case "xp menor que 1000":
+  console.log("menor que 1000")
+  break
+  case "xp for entre 2001 e 5000":
+  console.log("bronze")
+  case "xp for entre 5001 e 7000":
+  console.log("prata")}
+console.log("o herói de nome" + nome + "esta no nível de" + nivelDeHeroi)
+
+
